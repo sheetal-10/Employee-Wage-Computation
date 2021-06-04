695f09a4195f0337e9ae6870cacb2bc053a2b84d
sheetal-10 <sheetalp257@gmail.com>
2021-06-04 11:28:54 +0530

    employee wage using function

--- employeewagecomputation.sh
+++ employeewagecomputation.sh
@@ -6,9 +6,7 @@ workingDays=20
 maxHrsInMonth=100
 monthSalary=0
 totalWorkingHr=0
-for (( i=0; i<$workingDays && $totalWorkingHr<$maxHrsInMonth; i++ ))
-do
-	empCheck=$((RANDOM%3))
+function getWorkingHours() {
 	case $empCheck in
 		$isFullTimePresent)
 			workingHr=8
@@ -20,6 +18,13 @@ do
 			workingHr=0
 			;;
 	esac
+	echo $workingHr
+}
+
+for (( i=0; i<$workingDays && $totalWorkingHr<$maxHrsInMonth; i++ ))
+do
+	empCheck=$((RANDOM%3))
+	workingHr="$( getWorkingHours $empCheck )"
 	totalWorkingHr=$((totalWorkingHr+workingHr))
 done
 monthSalary=$((wagePerHour*totalWorkingHr))
