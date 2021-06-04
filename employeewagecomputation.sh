c1b7182168af87d8831d9dbabb9985cd779a1d95
sheetal-10 <sheetalp257@gmail.com>
2021-06-04 11:35:40 +0530

    Storing daily wage with total wage

--- employeewagecomputation.sh
+++ employeewagecomputation.sh
@@ -1,3 +1,4 @@
+declare -a dailyWage
 isPartTimePresent=2
 isFullTimePresent=1
 isAbsent=0
@@ -6,6 +7,7 @@ workingDays=20
 maxHrsInMonth=100
 monthSalary=0
 totalWorkingHr=0
+
 function getWorkingHours() {
 	case $empCheck in
 		$isFullTimePresent)
@@ -25,7 +27,9 @@ for (( i=0; i<$workingDays && $totalWorkingHr<$maxHrsInMonth; i++ ))
 do
 	empCheck=$((RANDOM%3))
 	workingHr="$( getWorkingHours $empCheck )"
+	dailyWage[i]=$((workingHr*wagePerHour))
 	totalWorkingHr=$((totalWorkingHr+workingHr))
 done
+echo "Employee Daily wage : ${dailyWage[@]}"
 monthSalary=$((wagePerHour*totalWorkingHr))
 echo "Employee monthly wage : $monthSalary"
